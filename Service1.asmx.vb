3ef85a2870944b3d4eade5b298fc689e1e246d4f
webservicehomework <sinister606@hotmail.com>
2016-03-21 15:43:35 -0600

    Update Service1.asmx.vb

--- Service1.asmx.vb
+++ Service1.asmx.vb
@@ -3,9 +3,6 @@ Imports System.Web.Services.Protocols
 Imports System.ComponentModel
 Imports System.Data.SqlClient
 
-
-' To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.
-'<System.Web.Script.Services.ScriptService()> _
 <System.Web.Services.WebService(Namespace:="http://localhost")> _
 <System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)> _
 <ToolboxItem(False)> _
